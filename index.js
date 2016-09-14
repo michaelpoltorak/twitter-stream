104a3f3c32fa2216ef68a6da562b8680fdc54506
michaelpoltorak <michaelpoltorak@gmail.com>
2016-09-14 23:54:43 +0200

    stuff

--- index.js
+++ index.js
@@ -1,56 +1,61 @@
-var express = require('express');
-var app = express();
-var http = require('http');
-var path = require('path');
-var jade = require('jade');
-var server = http.createServer(app);
-var io = require('socket.io')(server);
-var T = require('twit');
-var q = "twitter";
-var twit = new T({
-    // Add own Twitter credentials here:
-    consumer_key: '',
-    consumer_secret: '',
-    access_token: '',
-    access_token_secret: ''
+var express, app, http, path, jade, server, io, T, q, config, twit;
+
+express = require('express');
+app = express();
+http = require('http');
+path = require('path');
+jade = require('jade');
+server = http.createServer(app);
+io = require('socket.io')(server);
+T = require('twit');
+config = require('./config/config.js');
+q = 'twitter';
+
+twit = new T({
+    consumer_key:config.consumer_key,
+    consumer_secret: config.consumer_secret,
+    access_token: config.access_token,
+    access_token_secret: config.access_token_secret
 });
 
-//console.log('io ', io);
-
-app.use(express.static(__dirname + '/public'));
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-app.get('/page', function (req, res) {
-    //res.sendFile(__dirname + '/index.html');
-    res.render('index', {type:req.query.type});
-});
-
-server.listen(3002, function () {
-    console.log('listening on *:3002');
-});
 io.on('connection', function (socket) {
-    console.log('connection ', socket);
     var stream;
+    console.log('Got connection to client');
+    socket.on('disconnect', function () {
+        console.log('Client disconnected.');
+    });
     start();
     stream.on('tweet', function (tweet) {
-        console.log('tweet');
+        //console.log('tweet');
         io.emit('tweet', tweet);
     });
 
-    io.emit('message', {
-        for: 'everyone'
-    });
-    socket.on("newquery", function (newquery) {
-        q = newquery;
+    socket.on("query-updated", function (query) {
+        console.log('query-updated ', query);
+        q = query;
+        stream.stop();
         start();
     });
-
+    
     function start() {
-        console.log("start ", q);
-        stream = null;
+        console.log("Starting twitter streaming for keyword: ", q);
+        //stream = null;
         stream = twit.stream('statuses/filter', {
             track: [q]
         });
         io.emit('query', q);
     }
 });
+
+app.use(express.static(__dirname + '/public'));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.get('/page', function (req, res) {
+    //res.sendFile(__dirname + '/index.html');
+    res.render('index', { type: req.query.type });
+});
+
+server.listen(3000, function () {
+    console.log('listening on *:3000');
+});
+
--- public/js/app.js
+++ public/js/app.js
@@ -1,91 +1,94 @@
 
-var socket = io(),
-    oldcoord = [100, 100];
-//var container = document.getElementById("b");
+var socket = io();
+console.log('socket ', socket);
+// var container = document.getElementById("b");
+function setup () {
+  console.log('type ', type);
+  if (type === 'sunshine') {
+    sunshine();
+		//rain();
+	}
+	if (type === 'rain') {
+			rain();
+	}
+	if (type === '') {
+		stream();
+		var inp = document.getElementById('query');
+		inp.addEventListener("blur", function () {
+				socket.emit('query-updated', inp.value);
+		});
 
-
-function setup() {
-    console.log('type ', type);
-    if(type === 'sunshine') {
-        sunshine();
-        //rain();
-    }
-    if(type === 'rain') {
-        rain();
-    }
-    if(type === '') {
-        stream();
-    }
+	}
 }
-function stream() {
-    socket.on('tweet', function (tweet) {
-        console.log('tweet ', tweet.text);
-        
-        var el = document.getElementById('stream');
-        var old = el.innerHTML; 
-        el.innerHTML = '<div class="tweet">' + tweet.text + '</div>' + old; 
-    });
+function stream () {
+	socket.on('tweet', function (tweet) {
+		//console.log('tweet ', tweet.text);
+
+		var el = document.getElementById('stream');
+		var old = el.innerHTML;
+		el.innerHTML = '<div class="tweet">' + tweet.text + '</div>' + old;
+	});
 }
 function rain() {
-    console.log('draw rain');
-    createCanvas(720, 400);
-    stroke(16, 35, 81); // blueish
-    fill(102, 14, 14)
-    strokeWeight(10);
-    var th = 20;
-    var arr = [];
-    var i = 0;
-    //while (i < 5000) {
-    socket.on('tweet', function (tweet) {
-        console.log('draw tweet');
-        //var X = tweet.user.favourites_count;
-        var x = tweet.user.friends_count;
-        //var y = tweet.user.followers_count;
-        var y = tweet.user.favourites_count;
-        var Y1 = tweet.user.statuses_count;
-        //console.log(i);
-        var x = Math.random(500) * 1000;
-        var y = Math.random(500) * 1000;
-        arr.push({ x: x, y: y });
-        if (th > 0) {
-            th--;
-            line(x, 0, x + 30, y);
-        }
-        
-        if (th == 0) {
-            background(255);
-            console.log('th == 0');
-            arr.shift();
-            var j = 0;
-            while (j < arr.length) {
+	console.log('draw rain');
+	createCanvas(720, 400);
+	stroke(16, 35, 81); // blueish
+	fill(102, 14, 14)
+	strokeWeight(10);
+	var th = 20;
+	var arr = [];
+	var i = 0;
+	//while (i < 5000) {
+	socket.on('tweet', function (tweet) {
+		console.log('draw tweet');
+		//var X = tweet.user.favourites_count;
+		var x = tweet.user.friends_count;
+		//var y = tweet.user.followers_count;
+		var y = tweet.user.favourites_count;
+		var Y1 = tweet.user.statuses_count;
+		//console.log(i);
+		var x = Math.random(500) * 1000;
+		var y = Math.random(500) * 1000;
+		arr.push({ x: x, y: y });
+		if (th > 0) {
+			th--;
+			line(x, 0, x + 30, y);
+		}
 
-                line(arr[j].x, 0, arr[j].x + 30, arr[j].y);
-                j++;
-            }
-        }
-        //console.log('len ', arr.length, arr[arr.length - 1]);
-        //i++;
-    });
-    function updateRain() {
+		if (th == 0) {
+			background(255);
+			console.log('th == 0');
+			arr.shift();
+			var j = 0;
+			while (j < arr.length) {
 
-    }
+				line(arr[j].x, 0, arr[j].x + 30, arr[j].y);
+				j++;
+			}
+		}
+		//console.log('len ', arr.length, arr[arr.length - 1]);
+		//i++;
+	});
+	function updateRain() {
+
+	}
 }
 function sunshine() {
-    console.log('draw sunshine');
-    createCanvas(720, 400);
-    stroke(255, 204, 0); // yellowish
-    fill(255, 204, 0)
-    strokeWeight(10);
-    socket.on('tweet', function (tweet) {
-        console.log('draw tweet');
-        //var X = tweet.user.favourites_count;
-        var X = tweet.user.friends_count;
-        var Y = tweet.user.followers_count;
-        var X1 = tweet.user.favourites_count;
-        var Y1 = tweet.user.statuses_count;
-        background(0);
-        ellipse(X, Y, X1, Y1);
-        //circle(X);
-        oldcoord = [X, Y];
-    });
+	console.log('draw sunshine');
+	createCanvas(720, 400);
+	stroke(255, 204, 0); // yellowish
+	fill(255, 204, 0)
+	strokeWeight(10);
+	socket.on('tweet', function (tweet) {
+		console.log('draw tweet');
+		//var X = tweet.user.favourites_count;
+		var X = tweet.user.friends_count;
+		var Y = tweet.user.followers_count;
+		var X1 = tweet.user.favourites_count;
+		var Y1 = tweet.user.statuses_count;
+		background(0);
+		ellipse(X, Y, X1, Y1);
+		//circle(X);
+		oldcoord = [X, Y];
+	});
 }
